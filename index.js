500114f6cdfd8e442d78cae1b0534f5291ce3e6c
Ratan Narayan Hegde <63239964+RatanNarayanHegde@users.noreply.github.com>
2022-12-04 12:10:09 +0000

    add download image from url

--- index.js
+++ index.js
@@ -2,24 +2,40 @@ console.log("javascript working")
 
 const videos = [
     {
-        "url" : "https://www.youtube.com/watch?v=I2O7blSSzpI",
-        "photo": "photos/cicada.jpg"
+        "url": "https://www.youtube.com/watch?v=I2O7blSSzpI",
     },
     {
         "url": "https://www.youtube.com/watch?v=3fq83qbjPCM",
-        "photo": "photos/julianAssange.jpg"
     },
     {
         "url": "https://www.youtube.com/watch?v=oGVhOWqsBWM",
-        "photo": "photos/novaEffect.jpg"
+    },
+    {
+        "url": "https://www.youtube.com/watch?v=9vJRopau0g0",
     }
 ]
+
+const getVideoIdFromURL = (url) => {
+    const videoId = url.substring(32);
+
+    return videoId;
+}
+
+const getImageSrcFromURL = (url) => {
+    const videoId = getVideoIdFromURL(url);
+
+    const imgSrc = "https://img.youtube.com/vi/" + videoId + "/maxresdefault.jpg";
+
+    return imgSrc;
+}
 
 const div = document.querySelector("#content");
 
 const img = document.createElement("img");
-const idx = Math.floor(Math.random()* videos.length);
-img.src = videos[idx].photo;
+const idx = Math.floor(Math.random() * videos.length);
+img.src = getImageSrcFromURL(videos[idx].url);
+
+
 
 
 div.appendChild(img);
