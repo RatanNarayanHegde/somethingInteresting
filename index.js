a35d9aab1d7a0295683782c5c773357d437dbd24
Ratan Hegde <heyratan@amazon.com>
2023-08-20 17:37:22 +0530

    Add refresh button

--- index.js
+++ index.js
@@ -1,44 +1,50 @@
-console.log("javascript working")
+console.log("javascript working");
 
 const videos = [
-    {
-        "url": "https://www.youtube.com/watch?v=I2O7blSSzpI",
-    },
-    {
-        "url": "https://www.youtube.com/watch?v=3fq83qbjPCM",
-    },
-    {
-        "url": "https://www.youtube.com/watch?v=oGVhOWqsBWM",
-    },
-    {
-        "url": "https://www.youtube.com/watch?v=SSdsncLXLYs",
-    },
-    {
-        "url": "https://www.youtube.com/watch?v=EFkyxzJtiv4",
-    }
-]
+  {
+    url: "https://www.youtube.com/watch?v=I2O7blSSzpI",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=3fq83qbjPCM",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=oGVhOWqsBWM",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=SSdsncLXLYs",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=EFkyxzJtiv4",
+  },
+];
 
 const getVideoIdFromURL = (url) => {
-    const videoId = url.substring(32);
+  const videoId = url.substring(32);
 
-    return videoId;
-}
+  return videoId;
+};
 
 const getImageSrcFromURL = (url) => {
-    const videoId = getVideoIdFromURL(url);
+  const videoId = getVideoIdFromURL(url);
 
-    const imgSrc = "https://img.youtube.com/vi/" + videoId + "/maxresdefault.jpg";
+  const imgSrc = "https://img.youtube.com/vi/" + videoId + "/maxresdefault.jpg";
 
-    return imgSrc;
-}
+  return imgSrc;
+};
 
-const div = document.querySelector("#content");
+const appendVideoToDiv = () => {
+  const div = document.querySelector("#content");
 
-const img = document.createElement("img");
-const idx = Math.floor(Math.random() * videos.length);
-img.src = getImageSrcFromURL(videos[idx].url);
+  const img = document.createElement("img");
+  const idx = Math.floor(Math.random() * videos.length);
+  img.src = getImageSrcFromURL(videos[idx].url);
 
+  while (div.firstChild) div.removeChild(div.firstChild);
+  div.appendChild(img);
+};
 
+const changeVideo = () => {
+  appendVideoToDiv();
+};
 
-
-div.appendChild(img);
+appendVideoToDiv();
